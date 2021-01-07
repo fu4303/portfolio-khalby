386ed291e83033a44972bb2df9d2ac64da09f619
Khaleel Gibran <khalby786@gmail.com>
2021-01-07 19:29:08 +0400

    shoutout to someone

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <footer>
-        <a href="//blog.khaleelgibran.com"><p>blog</p></a>
+        <a href="//blog.khaleelgibran.com"><span>blog</span></a>&nbsp;·&nbsp;
+        <a href="//www.youtube.com/channel/UCXf1CMT59vnhMKi4nU0a9kQ"><span>also check out this cool youtube channel (not mine) 😉</span></a>
     </footer>
 </template>
 
