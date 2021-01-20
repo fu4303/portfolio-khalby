fe1c89621db5dd155f35e7437eb1b8056ff23db6
Khaleel Gibran <khalby786@gmail.com>
2021-01-20 13:22:03 +0400

    update social icons

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,9 +1,11 @@
 <template>
     <br /><br />
     <div>
+        <a href="//github.com/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/github.svg" /></a>
         <a href="//twitter.com/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/twitter-alt.svg" /></a>
         <a href="//instagram.com/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/instagram.svg" /></a>
-        <a href="//github.com/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/github.svg" /></a>
+        <a href="//twitch.tv/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/twitch.svg" /></a>
+        <a href="https://www.youtube.com/channel/UCrpLyU8D2wajlTae6qZx_eQ"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/youtube.svg" /></a>
         <a href="//codepen.io/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/codepen.svg" /></a>
         <a href="//reddit.com/u/khalby786"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/reddit.svg" /></a>
         <a href="mailto:khalby786@gmail.com"><img src="https://raw.githubusercontent.com/Automattic/social-logos/master/svg-min/mail.svg" /></a>
